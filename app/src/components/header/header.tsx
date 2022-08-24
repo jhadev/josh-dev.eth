c680ee61fd09265e6df67d002026d9a48de83ceb
Jhadev <joshappeldev@gmail.com>
2022-08-24 18:27:03 -0500

    small updates

--- app/src/components/header/header.tsx
+++ app/src/components/header/header.tsx
@@ -1,6 +1,6 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
-import styles from './header.css?inline';
+import { component$, useStyles$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import styles from "./header.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
@@ -11,22 +11,28 @@ export default component$(() => {
     <header>
       <div class="header-inner">
         <section class="logo">
-          <a href="/">Qwik City 🏙</a>
+          <a href="/">josh-dev</a>
         </section>
         <nav>
-          <a href="/blog" class={{ active: pathname.startsWith('/blog') }}>
+          <a href="/blog" class={{ active: pathname.startsWith("/blog") }}>
             Blog
           </a>
-          <a href="/docs" class={{ active: pathname.startsWith('/docs') }}>
+          <a href="/docs" class={{ active: pathname.startsWith("/docs") }}>
             Docs
           </a>
-          <a href="/api" class={{ active: pathname.startsWith('/api') }}>
+          <a href="/api" class={{ active: pathname.startsWith("/api") }}>
             API
           </a>
-          <a href="/products/hat" class={{ active: pathname.startsWith('/products') }}>
+          <a
+            href="/products/hat"
+            class={{ active: pathname.startsWith("/products") }}
+          >
             Products
           </a>
-          <a href="/about-us" class={{ active: pathname.startsWith('/about-us') }}>
+          <a
+            href="/about-us"
+            class={{ active: pathname.startsWith("/about-us") }}
+          >
             About Us
           </a>
         </nav>
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,16 +1,21 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <div>
-      <h1>Welcome to Qwik City</h1>
+      <h1>Welcome</h1>
 
-      <p>The meta-framework for Qwik.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        consequatur beatae tempora natus iste deserunt distinctio maxime ipsa
+        cumque, minus officiis facilis molestias doloremque quibusdam corrupti
+        sint quaerat incidunt voluptas.
+      </p>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik City',
+  title: "Josh A - Web Performance Engineer"
 };
